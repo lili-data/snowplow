1c197198dfa2e71040a98be6ea622fbd7a82a3a1
Tamas Szuromi <tamas.szuromi@edreamsodigeo.com>
2020-04-17 14:33:24 +0200

    adding perf timing and user agents

--- macros/adapters/bigquery/pageviews/snowplow_page_views.sql
+++ macros/adapters/bigquery/pageviews/snowplow_page_views.sql
@@ -73,6 +73,159 @@ events as (
 
 ),
 
+prep as (
+
+    select
+        page_view_id,
+        -- More information on performance timing https://discourse.snowplowanalytics.com/t/measuring-page-load-times-with-the-performance-timing-context-tutorial/100
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.navigationStart") AS INT64) AS navigation_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.redirectStart") AS INT64) AS redirect_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.redirectEnd") AS INT64) AS redirect_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.fetchStart") AS INT64) AS fetch_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domainLookupStart") AS INT64) AS domain_lookup_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domainLookupEnd") AS INT64) AS domain_lookup_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.secureConnectionStart") AS INT64) AS secure_connection_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.connectStart") AS INT64) AS connect_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.connectEnd") AS INT64) AS connect_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.requestStart") AS INT64) AS request_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.responseStart") AS INT64) AS response_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.responseEnd") AS INT64) AS response_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.unloadEventStart") AS INT64) AS unload_event_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.unloadEventEnd") AS INT64) AS unload_event_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domLoading") AS INT64) AS dom_loading,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domInteractive") AS INT64) AS dom_interactive,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domContentLoadedEventStart") AS INT64) AS dom_content_loaded_event_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domContentLoadedEventEnd") AS INT64) AS dom_content_loaded_event_end,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.domComplete") AS INT64) AS dom_complete,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.loadEventStart") AS INT64) AS load_event_start,
+        safe_cast(`liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(contexts, "$.data[?(@.schema == 'iglu:org.w3/PerformanceTiming/jsonschema/1-0-0')].data.loadEventEnd") AS INT64) AS load_event_end
+
+    from events as pt
+
+),
+
+rolledup AS (
+
+    select
+        page_view_id,
+
+        -- select the first non-zero value
+        min(nullif(navigation_start, 0)) as navigation_start,
+        min(nullif(redirect_start, 0)) as redirect_start,
+        min(nullif(redirect_end, 0)) as redirect_end,
+        min(nullif(fetch_start, 0)) as fetch_start,
+        min(nullif(domain_lookup_start, 0)) as domain_lookup_start,
+        min(nullif(domain_lookup_end, 0)) as domain_lookup_end,
+        min(nullif(secure_connection_start, 0)) as secure_connection_start,
+        min(nullif(connect_start, 0)) as connect_start,
+        min(nullif(connect_end, 0)) as connect_end,
+        min(nullif(request_start, 0)) as request_start,
+        min(nullif(response_start, 0)) as response_start,
+        min(nullif(response_end, 0)) as response_end,
+        min(nullif(unload_event_start, 0)) as unload_event_start,
+        min(nullif(unload_event_end, 0)) as unload_event_end,
+        min(nullif(dom_loading, 0)) as dom_loading,
+        min(nullif(dom_interactive, 0)) as dom_interactive,
+        min(nullif(dom_content_loaded_event_start, 0)) as dom_content_loaded_event_start,
+        min(nullif(dom_content_loaded_event_end, 0)) as dom_content_loaded_event_end,
+        min(nullif(dom_complete, 0)) as dom_complete,
+        min(nullif(load_event_start, 0)) as load_event_start,
+        min(nullif(load_event_end, 0)) as load_event_end
+
+    from prep
+    -- all values should be set and some have to be greater than 0 (not the case in about 1% of events)
+    where navigation_start is not null and navigation_start > 0
+      and redirect_start is not null -- zero is acceptable
+      and redirect_end is not null -- zero is acceptable
+      and fetch_start is not null and fetch_start > 0
+      and domain_lookup_start is not null and domain_lookup_start > 0
+      and domain_lookup_end is not null and domain_lookup_end > 0
+      and secure_connection_start is not null and secure_connection_start > 0
+      and connect_end is not null and connect_end > 0
+      and request_start is not null and request_start > 0
+      and response_start is not null and response_start > 0
+      and response_end is not null and response_end > 0
+      and unload_event_start is not null -- zero is acceptable
+      and unload_event_end is not null -- zero is acceptable
+      and dom_loading is not null and dom_loading > 0
+      and dom_interactive is not null and dom_interactive > 0
+      and dom_content_loaded_event_start is not null and dom_content_loaded_event_start > 0
+      and dom_content_loaded_event_end is not null and dom_content_loaded_event_end > 0
+      and dom_complete is not null -- zero is acceptable
+      and load_event_start is not null -- zero is acceptable
+      and load_event_end is not null -- zero is acceptable
+    group by 1
+
+),
+
+perf_timing AS (
+
+select
+    page_view_id,
+
+    case
+        when ((redirect_start is not null) and (redirect_end is not null) and (redirect_end >= redirect_start)) then (redirect_end - redirect_start)
+        else null
+    end as redirect_time_in_ms,
+
+    case
+        when ((unload_event_start is not null) and (unload_event_end is not null) and (unload_event_end >= unload_event_start)) then (unload_event_end - unload_event_start)
+        else null
+    end as unload_time_in_ms,
+
+    case
+        when ((fetch_start is not null) and (domain_lookup_start is not null) and (domain_lookup_start >= fetch_start)) then (domain_lookup_start - fetch_start)
+        else null
+    end as app_cache_time_in_ms,
+
+    case
+        when ((domain_lookup_start is not null) and (domain_lookup_end is not null) and (domain_lookup_end >= domain_lookup_start)) then (domain_lookup_end - domain_lookup_start)
+        else null
+    end as dns_time_in_ms,
+
+    case
+        when ((connect_start is not null) and (connect_end is not null) and (connect_end >= connect_start)) then (connect_end - connect_start)
+        else null
+    end as tcp_time_in_ms,
+
+    case
+        when ((request_start is not null) and (response_start is not null) and (response_start >= request_start)) then (response_start - request_start)
+        else null
+    end as request_time_in_ms,
+
+    case
+        when ((response_start is not null) and (response_end is not null) and (response_end >= response_start)) then (response_end - response_start)
+        else null
+    end as response_time_in_ms,
+
+    case
+        when ((dom_loading is not null) and (dom_complete is not null) and (dom_complete >= dom_loading)) then (dom_complete - dom_loading)
+        else null
+    end as processing_time_in_ms,
+
+    case
+        when ((dom_loading is not null) and (dom_interactive is not null) and (dom_interactive >= dom_loading)) then (dom_interactive - dom_loading)
+        else null
+    end as dom_loading_to_interactive_time_in_ms,
+
+    case
+        when ((dom_interactive is not null) and (dom_complete is not null) and (dom_complete >= dom_interactive)) then (dom_complete - dom_interactive)
+        else null
+    end as dom_interactive_to_complete_time_in_ms,
+
+    case
+        when ((load_event_start is not null) and (load_event_end is not null) and (load_event_end >= load_event_start)) then (load_event_end - load_event_start)
+        else null
+    end as onload_time_in_ms,
+
+    case
+        when ((navigation_start is not null) and (load_event_end is not null) and (load_event_end >= navigation_start)) then (load_event_end - navigation_start)
+        else null
+    end as total_time_in_ms
+
+from rolledup
+),
+
 page_views as (
 
   select
@@ -161,9 +314,6 @@ page_views as (
 
     br_lang as browser_language,
 
-    -- TODO : useragent
-    -- TODO : perf_timing
-
     struct(
         `liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(derived_contexts, "$.data[?(@.schema == 'iglu:nl.basjes/yauaa_context/jsonschema/1-0-0')].data.agentName") as browser_engine,
         `liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(derived_contexts, "$.data[?(@.schema == 'iglu:nl.basjes/yauaa_context/jsonschema/1-0-0')].data.deviceClass") as type,
@@ -171,11 +321,11 @@ page_views as (
         `liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(derived_contexts, "$.data[?(@.schema == 'iglu:nl.basjes/yauaa_context/jsonschema/1-0-0')].data.deviceName") as name,
         `liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(derived_contexts, "$.data[?(@.schema == 'iglu:nl.basjes/yauaa_context/jsonschema/1-0-0')].data.webviewAppName") as app_name,
         `liligo-conversion-237810`.analytics.CUSTOM_JSON_EXTRACT(derived_contexts, "$.data[?(@.schema == 'iglu:nl.basjes/yauaa_context/jsonschema/1-0-0')].data.agentName") as agent
-    ) as device
+    ) as device,
 
     case
       when br_family = 'Robot/Spider'
-          or {% set bad_agents_psv = bot_any()|join('|') %}
+       or {% set bad_agents_psv = bot_any()|join('|') %}
           not regexp_contains(LOWER(useragent), '^.*({{bad_agents_psv}}).*$')
           or useragent is null then TRUE
       else FALSE
@@ -282,5 +432,6 @@ engagement as (
 select *
 from page_views
 join engagement using (page_view_id)
+join perf_timing using (page_view_id)
 
 {% endmacro %}
