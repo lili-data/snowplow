baeeb459f27dd8506f4dca78c38e3ac12e6a0ce5
Tamas Szuromi <tamas.szuromi@edreamsodigeo.com>
2020-04-20 11:58:45 +0200

    lightweight sessions

--- macros/adapters/bigquery/sessions/snowplow_sessions.sql
+++ macros/adapters/bigquery/sessions/snowplow_sessions.sql
@@ -15,27 +15,11 @@ with sessions as (
 
 ),
 
-id_map as (
 
-    select * from {{ ref('snowplow_id_map') }}
-
-),
-
-
-stitched as (
-
-    select
-        coalesce(id.user_id, s.user_snowplow_domain_id) as inferred_user_id,
-        s.*
-
-    from sessions as s
-    left outer join id_map as id on s.user_snowplow_domain_id = id.domain_userid
-
-)
 
 select
     * except(session_index),
-    row_number() over (partition by inferred_user_id order by session_start) as session_index,
+    row_number() over (partition by user_snowplow_domain_id order by session_start) as session_index,
     DATE(session_start) as dt
 
 from stitched
